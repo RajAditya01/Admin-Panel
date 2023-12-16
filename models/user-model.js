9df741f956ad058d93959d949a775e2c397070b7
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-12-16 20:36:22 +0530

    .

--- models/user-model.js
+++ models/user-model.js
@@ -25,4 +25,6 @@ const userSchema = new mongoose.Schema({
 });
 
 // define the model or the collection name
-export const User = new mongoose.model("USER", userSchema);
+const User = mongoose.model("USER", userSchema);
+
+module.exports = User;  // Use module.exports instead of export
