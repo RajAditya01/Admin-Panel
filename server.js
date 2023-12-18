c610bd1b2e930c47afcb2c158eb12fbf2ecd5fc4
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-12-18 18:31:44 +0530

    contact

--- server.js
+++ server.js
@@ -1,32 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const app = express();
-// const router = require("./routes/auth-router");
-// const connectDb = require("./utils/db");
-// const errorMiddleware = require("./middlewares/error-middleware");
-// const contactRoute = require("./routes/contact-route");
-// // Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
-// app.use("/api/auth", router);
-
-// const PORT = 5000;
-
-// connectDb().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`server is running at port: ${PORT}`);
-//   });
-// });
-
-// app.all("*", (req, res) => {
-//   res.send('Route Not Defined')
-// })
-
-// app.listen(PORT, () => {
-//   console.log('Server Up')
-// })
-
-
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
