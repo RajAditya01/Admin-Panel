4336a5ca17fb9fb1453fcd9592a592b95f8b54f6
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-12-22 09:15:01 +0530

    auth-register

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authControllers = require("../Controllers/auth-controller"); // Adjusted path
 
 router.route("/").get(authControllers.home);
-router.route("/register").get(authControllers.register);
-router.route("/login").get(authControllers.login);
+router.route("/register").post(authControllers.register);
+router.route("/login").post(authControllers.login);
 module.exports = router;
